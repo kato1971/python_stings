2e1a7201ad016ed8ed5f33c04789afb50694d16d
kato1971 <88402289+kato1971@users.noreply.github.com>
2022-05-25 15:02:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,28 +12,30 @@ goal_0 = 32
 goal_1 = 54
 
 scorers = scorer_0 + " " + str(goal_0) + ", " + scorer_1 + " " + str(goal_1)
+print(scorers)
 
-report = scorer_0 + " " + "scored in the" + " " + str(goal_0) + "nd minute" + "\n" + scorer_1 + " " + "scored in the" + " " + str(goal_1) + "th minute"
+report = f"{scorer_0} scored in the {goal_0}nd minute\n{scorer_1} scored in the {goal_1}th minute"
+print(report)
 
 """ PART 2 """
 player = "Frank Rijkaard"
 
-find_first_name = player.find("Frank")
-first_name = player[:5]
+first_name = player[:player.find(" ")]
 print(first_name)
 
-find_last_name = player.find("Rijkaard")
-last_name_len = len(player[6:])
+last_name = player[player.find(" ") +1:]
+print(last_name)
+last_name_len = len(last_name)
 print(last_name_len)
 
-name_short = player.replace("Frank", "F.")
+name_short = f"{player[0]}. {last_name}"
 print(name_short)
 
-first_name_len = len(player[:5])
+first_name_len = len(first_name)
 chant_1 = (first_name + "!" + " ") * first_name_len
 chant = chant_1[:-1]
+
 print(chant)
 
-good_chant =  chant != 3
+good_chant = chant[-1] != " "
 print(good_chant)
-print(chant != 2)
